2290ea379af77b675f4b8a067bb41e28356a6e10
fdemirciler <fdemirciler@gmail.com>
2021-05-31 02:01:44 +0300

    Answers added.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,26 @@
-const repeatString = function() {
+// const helloWorld = require("../helloWorld/helloWorld");
 
+const repeatString = function (word, num) {
+
+    let newWord = new String;
+
+    if (num < 0) {
+
+        return 'ERROR'
+
+    } else if (num === 0) {
+
+        return ''
+
+    }
+
+
+    for (let i = 0; i < num; i++) {
+
+        newWord += word
+
+    }
+    return newWord
 };
 
 module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function (str) {
 
+    let newString = new String;
+
+    if (str.length === 0) {
+
+        return ''
+    }
+
+    for (let i = str.length - 1; i >= 0; i--) {
+
+        newString += str[i]
+
+    }
+
+    return newString
 };
 
 module.exports = reverseString;
